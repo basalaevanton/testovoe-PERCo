ccfdca4d5fc6b00732ab5e3e516e96c551bf49fc
basalaevanton <74416144+basalaevanton@users.noreply.github.com>
2021-12-19 20:05:04 +0300

    fix modal style

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -9,7 +9,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 1000,
+  // width: 1000,
   borderRadius: "15px",
   bgcolor: "background.paper",
   border: "2px solid #000",
